2f7edc4318edefcee77325e873dc71a883598138
mcquo011 <omcquown@gmail.com>
2023-04-25 11:01:32 -0500

    Completed install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Store the event so it can be triggered later
+  window.deferredPrompt = event;
+  // Update UI notify the user they can add to home screen
+  butInstall.style.display = "block";
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+butInstall.addEventListener('click', async () => {
+  // Hide our user interface that shows our A2HS button
+  butInstall.style.display = "none";
+  // Show the prompt
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await promptEvent.userChoice;
+  console.log(`User response to the install prompt: ${outcome}`);
+  // Clear stored prompt since it can't be used again
+  window.deferredPrompt = null;
+});
+
+
+window.addEventListener('appinstalled', (event) => {
+  console.log("App installed successfully:", event);
+});
