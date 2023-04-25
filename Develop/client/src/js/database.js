c8023a04523fa28375e7c3812907c64016a42599
mcquo011 <omcquown@gmail.com>
2023-04-25 12:29:40 -0500

    putDb logic

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,6 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+    const db = await initdb();
+    const tx = db.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    await store.add(content);
+    await tx.done;
+    console.log("Content added to the database:", content);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
